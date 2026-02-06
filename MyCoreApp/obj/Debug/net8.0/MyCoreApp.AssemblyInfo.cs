2c0119d3628ad1d30cb350e27c175dfa5e6f9180
Ripudaman Singh <rsin1191@my.centennialcollege.ca>
2026-02-07 01:59:20 +0530

    Update greeting and add gitignore

--- MyCoreApp/obj/Debug/net8.0/MyCoreApp.AssemblyInfo.cs
+++ MyCoreApp/obj/Debug/net8.0/MyCoreApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bc725a35a85862ec82c95a95f1f7ab31da7b32d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
